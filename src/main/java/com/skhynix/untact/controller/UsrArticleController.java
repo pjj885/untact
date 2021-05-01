4c47ef5495cdfcc35e735f9e95d732e2185d202a
pjj885 <xorhkd513@naver.com>
2021-05-01 17:26:21 +0900

    게시물 삭제

--- src/main/java/com/skhynix/untact/controller/UsrArticleController.java
+++ src/main/java/com/skhynix/untact/controller/UsrArticleController.java
@@ -6,18 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class UsrArticleController {
-
+    private int articlesLastId;
     private List<Article> articles;
 
     public UsrArticleController() {
+        articlesLastId = 0;
         articles = new ArrayList<>();
 
-        articles.add(new Article(1, "2021-05-01", "제목1", "내용1"));
-        articles.add(new Article(2, "2021-05-01", "제목2", "내용2"));
+        // 게시물 2개 생성
+        articles.add(new Article(++articlesLastId, "2021-05-01", "제목1", "내용1"));
+        articles.add(new Article(++articlesLastId, "2021-05-01", "제목2", "내용2"));
     }
 
     @RequestMapping("usr/article/detail")
@@ -28,8 +32,52 @@ public class UsrArticleController {
 
     @RequestMapping("usr/article/list")
     @ResponseBody
-    public List<Article> showList(){
+    public List<Article> showList() {
         return articles;
     }
 
+    @RequestMapping("/usr/article/doAdd")
+    @ResponseBody
+    public Map<String, Object> doAdd(String regDate, String title, String body){
+        articles.add(new Article(++articlesLastId, regDate, title, body));
+
+        Map<String, Object> rs = new HashMap<>();
+        rs.put("resultCode", "S-1");
+        rs.put("msg", "성공하였습니다.");
+        rs.put("id", articlesLastId);
+
+        return rs;
+    }
+
+    @RequestMapping("/usr/article/doDelete")
+    @ResponseBody
+    public Map<String, Object> doDelete(int id){
+
+        boolean deleteArticleRS = deleteArticle(id);
+
+        Map<String, Object> rs = new HashMap<>();
+
+        if (deleteArticleRS){
+        rs.put("resultCode", "S-1");
+        rs.put("msg", "성공하였습니다.");
+        }
+        else {
+        rs.put("resultCode", "F-1");
+        rs.put("msg", "해당 게시물이 존재하지 않습니다.");
+            
+        }
+        rs.put("id", id);
+
+        return rs;
+    }
+
+    private boolean deleteArticle(int id) {
+        for (Article article : articles) {
+            if (article.getId() == id) {
+                articles.remove(article);
+                return true;
+            }
+        }
+        return false;
+    }
 }
