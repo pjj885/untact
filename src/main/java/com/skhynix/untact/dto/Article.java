2fd0d3be698172a97025d8483a440ef9dbbb8ca2
pjj885 <xorhkd513@naver.com>
2021-05-01 22:59:38 +0900

    lombok 적용

--- src/main/java/com/skhynix/untact/dto/Article.java
+++ src/main/java/com/skhynix/untact/dto/Article.java
@@ -1,5 +1,12 @@
 package com.skhynix.untact.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Article {
 
     private int id;
@@ -13,51 +20,6 @@ public class Article {
         this.regDate = regDate;
         this.updateDate = updateDate;
         this.title = title;
-        this.body = body;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" + "id=" + id + ", regDate='" + regDate + '\'' + ", updateDate='" + updateDate + '\'' + ", title='" + title + '\'' + ", body='" + body + '\'' + '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(String regDate) {
-        this.regDate = regDate;
-    }
-
-    public String getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(String updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
         this.body = body;
     }
 }
